3a1ae9055223efcc66ee526b703642cfa3167c6e
吴志聪 <wuzhicong@MacBook-Pro-3.local>
2019-08-19 22:54:08 +0800

    feat: use flex layout

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,14 @@ import './header.css';
 
 export default class Header extends React.Component {
   render() {
-    return <div className="header">
-      <div className="header-left">Itgo</div>
-      <div className="header-right">
-        <img src={githublogo} className="github-icon" alt={"git"} />
+    return <>
+      <div className="header">
+        <div className="logo">Itgo</div>
+        <a className="description" href="https://github.com/weboutin">
+          <img src={githublogo} className="github-icon" alt={"git"} />
+        </a>
       </div>
-    </div>
+      <div className="headerEmpty"></div>
+    </>
   }
 }
